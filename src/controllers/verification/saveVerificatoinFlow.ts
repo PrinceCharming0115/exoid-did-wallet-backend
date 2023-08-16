047ef32591fe2b50eb6df534cdd7d2a78ae0e590
arturdranhoi <artur.dranhoi@exiod.io>
2023-08-16 15:44:33 +0200

    refactor: change file name

--- src/controllers/verification/saveVerificatoinFlow.ts
+++ src/controllers/verification/saveVerificatoinFlow.ts
@@ -1,21 +1,17 @@
-import { Request, Response, response } from 'express';
-
-import httpStatus from 'http-status';
 import { REASON_CODES } from 'consts';
-import { CustomError } from 'errors';
-
 import 'dotenv/config';
-
+import { CustomError } from 'errors';
+import { Request, Response, response } from 'express';
+import httpStatus from 'http-status';
 import { verificationService } from 'services';
-
-import { errorHandlerWrapper } from 'utils';
 import { AuthRequest } from 'types';
+import { errorHandlerWrapper } from 'utils';
 
 export const saveVerificatonFlowValidator = () => {
-	return [];
-}
+  return [];
+};
 
-type Params= unknown;
+type Params = unknown;
 type ResBody = unknown;
 type ReqBody = {
   verificationFlowName: string;
@@ -24,30 +20,30 @@ type ReqBody = {
 type ReqQuery = unknown;
 
 export const saveVerificationFlowHandler = async (
-	req: AuthRequest<Params, ResBody, ReqBody, ReqQuery>,
-	res: Response
+  req: AuthRequest<Params, ResBody, ReqBody, ReqQuery>,
+  res: Response
 ) => {
+  const { verificationFlow, verificationFlowName } = req.body;
 
-  const { verificationFlow, verificationFlowName } = req.body
-  
-	const did = req.account.identifier;
+  const did = req.account.identifier;
 
   const verification = await verificationService.saveVerification({
     did,
     verificationFlowName,
-    verificationFlow
+    verificationFlow,
   });
 
   if (!verification) {
     throw new CustomError(
       'Unknown Error',
-      httpStatus.BAD_REQUEST, 
+      httpStatus.BAD_REQUEST,
       REASON_CODES.EMAIL.UNKNOW_ERROR
     );
   }
 
-  res.status(httpStatus.CREATED).json("Success");
+  res.status(httpStatus.CREATED).json('Success');
+};
 
-}
-
-export const saveVerification = errorHandlerWrapper(saveVerificationFlowHandler);
+export const saveVerification = errorHandlerWrapper(
+  saveVerificationFlowHandler
+);
